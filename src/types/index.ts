9b342b57ff457db23281aa21c31dc579ab4d6965
Rajeswar <50909821+rajeswarsingh@users.noreply.github.com>
2026-02-10 20:55:06 +0530

    Create TypeScript type definitions for various interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,45 @@
-export * from "./config.types";
-export * from "./content.types";
+// TypeScript type definitions for various interfaces
+
+export interface About {
+    title: string;
+    description: string;
+    image: string;
+}
+
+export interface Blog {
+    id: string;
+    title: string;
+    content: string;
+    date: Date;
+}
+
+export interface Gallery {
+    images: string[];
+}
+
+export interface Home {
+    welcomeMessage: string;
+    featuredProjects: string[];
+}
+
+export interface Newsletter {
+    email: string;
+    subscribeDate: Date;
+}
+
+export interface Person {
+    name: string;
+    age: number;
+    role: string;
+}
+
+export interface Social {
+    platform: string;
+    url: string;
+}
+
+export interface Work {
+    title: string;
+    description: string;
+    tools: string[];
+}
